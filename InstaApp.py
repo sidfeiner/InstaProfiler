cc7eb1d40004465e154c403b7557188d108dae02
Sid Feiner <sidney.feiner@startapp.com>
2017-04-16 00:06:12 +0300

    Remove test log, added trailing slash to route

--- InstaApp.py
+++ InstaApp.py
@@ -8,10 +8,10 @@ from LoggerManager import logger
 __author__ = 'Sidney'
 
 
-@app.route('/auth')
+@app.route('/auth/')
 def auth():
     code = request.args.get('code')
-    # redirected from: https://api.instagram.com/oauth/authorize/?client_id=1ce2ad36a097486984642c7d6db041ed&redirect_uri=http%3A%2F%2Fec2-54-71-98-189.us-west-2.compute.amazonaws.com%3A9000%2Fauth&scope=basic+follower_list+comments+relationships+public_content+likes&response_type=code
+    # redirected from: https://api.instagram.com/oauth/authorize/?client_id=1ce2ad36a097486984642c7d6db041ed&redirect_uri=http%3A%2F%2Fec2-54-71-98-189.us-west-2.compute.amazonaws.com%3A9000%2Fauth%2F&scope=basic+follower_list+comments+relationships+public_content+likes&response_type=code
     try:
         token = insta_api.get_access_token(code=code, redirect_url=request.url)
         token.modify(
@@ -29,5 +29,4 @@ def auth():
         })
 
 
-logger.info("oh my god")
 app.run(host=constants.flask_host, port=constants.flask_port)
