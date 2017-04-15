7a08a40d1ac788968e6b6a156166ed17f6811201
Sid Feiner <sidney.feiner@startapp.com>
2017-04-15 16:49:19 +0300

    Add prints for LoggerManager

--- LoggerManager.py
+++ LoggerManager.py
@@ -17,10 +17,14 @@ def init_file_handler(logger: logging.Logger, encoding="utf-8"):
     """
     file_name = "{name}-{date}".format(name=app.name, date=datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
     file_path = os.path.join(logs_dir, file_name)
+    if not os.path.exists(logs_dir):
+        os.makedirs(logs_dir)
     handler = logging.FileHandler(file_path, encoding=encoding)
     handler.setFormatter(format)
     logger.addHandler(handler)
 
 logger = logging.getLogger("InstaProfiler")
 format = logging.Formatter("%(asctime)s | %(levelno)s | $(module)s | %(lineno)s | %(message)s")
+print("Initiating handler")
 init_file_handler(logger)
+print("Handler initiated")
